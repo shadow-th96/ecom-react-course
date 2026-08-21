a4844f784a30f7c4612334d3d2ceda32785b103e
shadow-th96 <itsthiha.hunter@gmail.com>
2026-08-21 14:10:17 +0700

    Added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import Auth from "./pages/Auth";
 import NavBar from "./components/NavBar";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="app">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
